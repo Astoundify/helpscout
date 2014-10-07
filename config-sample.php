c62d4f61f928c3a954d42e715984def2b368717d
Spencer Finnell <spencerfinnell@gmail.com>
2014-10-07 12:22:34 -0400

    Update config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,6 @@
+define( 'HELPSCOUT_SECRET_KEY', '' );
+define( 'HELPSCOUT_EMAIL', '' );
+define( 'HELPSCOUT_SUPPORT_API_KEY', '' );
+
+define( 'ENVATO_USERNAME', '' );
+define( 'ENVATO_API_KEY', '' );
